709e6046b1be60205f17eb7c5b821536dbc18659
Nick Rosier <github@bunbun.be>
2020-12-29 22:55:15 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+let distance = 0
+basic.forever(function () {
+    distance = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distance)
+})
